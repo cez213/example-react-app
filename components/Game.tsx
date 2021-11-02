5b852d6409ab46b8875431d8cce9195b252e840b
cez213 <carolyn.zelenetz@gmail.com>
2021-11-02 18:05:46 -0400

    1 file modified

--- components/Game.tsx
+++ components/Game.tsx
@@ -5,9 +5,9 @@ import { Board } from './Board';
 export const Game = ({ value, onClick }) => {
   const [board, setBoard] = React.useState(Array(9).fill(null));
   const [xTurn, setNextTurn] = React.useState(true);
-  const winner = calculateWinner();
+  const winner = calculateWinner(board);
 
-  function calculateWinner() {
+  function calculateWinner(board) {
     const lines = [
       [0, 1, 2],
       [0, 3, 6],
@@ -18,6 +18,17 @@ export const Game = ({ value, onClick }) => {
       [3, 4, 5],
       [6, 7, 8],
     ];
+
+    for (const line of lines) {
+      const currentMarker = board[line[0]];
+      if (
+        board[line[0]] === board[line[1]] &&
+        board[line[1]] === board[line[2]] &&
+        currentMarker !== null
+      ) {
+        return currentMarker;
+      }
+    }
   }
 
   return (
